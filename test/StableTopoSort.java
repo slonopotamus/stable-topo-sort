b649dfb5d0a2946d0e93cd3da90c296d04d04bd6
Marat Radchenko <marat@slonopotamus.org>
2019-05-10 23:17:01 +0300

    Add link to article from code

--- test/StableTopoSort.java
+++ test/StableTopoSort.java
@@ -10,6 +10,7 @@ import java.util.*;
  * https://github.com/DavePearce/StronglyConnectedComponents/blob/master/src/PeaFindScc2.java
  * https://github.com/gonum/gonum/blob/master/graph/topo/tarjan.go
  * http://homepages.ecs.vuw.ac.nz/~djp/files/IPL15-preprint.pdf
+ * https://habr.com/ru/post/451208/
  */
 public final class StableTopoSort {
 
